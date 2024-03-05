e2104f98e6ea186ccc41455347ec06cca66c43d0
unknown <abdallaatef12@gmail.com>
2024-03-05 18:04:13 +0200

    adding Authontication to rooms

--- src/EndPoints/rooms/rooms.controller.ts
+++ src/EndPoints/rooms/rooms.controller.ts
@@ -3,11 +3,13 @@ import { RoomsService } from './rooms.service';
 import { CreateRoomDto } from './dto/create-room.dto';
 import { UpdateRoomDto } from './dto/update-room.dto';
 import { RoomWithDto } from './dto/rooms.dto';
+import { UserRoles } from '../auth/roles.decorator';
+import { Role } from '../auth/roles.enum';
 
 @Controller('rooms')
 export class RoomsController {
   constructor(private readonly roomsService: RoomsService) { }
-
+  @UserRoles(Role.Admin)
   @UsePipes(ValidationPipe)
   @Post()
   create(@Body() room: RoomWithDto) {
@@ -33,13 +35,13 @@ export class RoomsController {
   }
 
 
-
+  @UserRoles(Role.Admin)
   @UsePipes(ValidationPipe)
   @Patch(':id')
   update(@Param('id') id: string, @Body() room: RoomWithDto) {
     return this.roomsService.update(+id, room);
   }
-
+  @UserRoles(Role.Admin)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.roomsService.remove(+id);
