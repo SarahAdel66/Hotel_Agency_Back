6ec477477dee03a9523be1ea959898722d7eab39
unknown <abdallaatef12@gmail.com>
2024-03-11 03:56:34 +0200

    changing reservation logic

--- src/EndPoints/reservation/reservation.service.ts
+++ src/EndPoints/reservation/reservation.service.ts
@@ -8,29 +8,65 @@ import { RoomsService } from '../rooms/rooms.service';
 export class ReservationService {
   constructor(@InjectModel('reservations') private ReservationModel,private  roomService:RoomsService) {}
 
-  private async reserve(createReservationDto:CreateReservationDto,roomToReserve:any){
+  private async reserve(createReservationDto:CreateReservationDto){
+    // private async reserve(createReservationDto:CreateReservationDto,roomToReserve:any){
     var maxIdReservation = await this.ReservationModel.findOne({}, { _id: 1 }, { sort: { _id: -1 } });
     var maxId = maxIdReservation ? maxIdReservation._id : 0;
     createReservationDto._id = maxId+1;
     let newReservation = new this.ReservationModel(createReservationDto);
     await newReservation.save();
-    roomToReserve.quantity -= createReservationDto.roomsNo;
-    console.log(roomToReserve);
-    await this.roomService.update(createReservationDto.roomId, roomToReserve);
+    // roomToReserve.quantity -= createReservationDto.roomsNo;
+    // console.log(roomToReserve);
+    // await this.roomService.update(createReservationDto.roomId, roomToReserve);
     return newReservation;
   }
-
-  async create(createReservationDto: CreateReservationDto) {
-    let roomToReserve = await this.roomService.findOne(createReservationDto.roomId);
-    console.log(roomToReserve);    
-    let willBeAvailable = roomToReserve.quantity;
-    if(roomToReserve.quantity < createReservationDto.roomsNo){
-      let validRooms= await this.ReservationModel.find({checkOutDate :{$lte : createReservationDto.checkInDate},roomId:createReservationDto.roomId})
-      if(validRooms.length === 0)return {message:"Room Not Available At this time"}
-      validRooms.forEach((element:CreateReservationDto) => {willBeAvailable+= element.roomsNo})
+  private async checkAvailability(createReservationDto:any){
+    let roomToReserve = await this.roomService.findOne(createReservationDto.roomId); 
+    // console.log(createReservationDto);
+    let reservationForThisRoom = await this.ReservationModel.find({roomId:createReservationDto.roomId});
+    console.log(reservationForThisRoom);
+    
+    if(roomToReserve.quantity  < createReservationDto.roomsNo)return {message:`we only have ${roomToReserve.quantity}  rooms from  this type`}
+    if(reservationForThisRoom.length !== 0){
+      let validRooms= await this.ReservationModel.find({checkOutDate :{$lte : createReservationDto.checkInDate,$ne : createReservationDto.checkInDate},roomId:createReservationDto.roomId})
+      let currentlyReserved = 0;   
+      // console.log(reservationForThisRoom);
+      reservationForThisRoom.forEach((element:CreateReservationDto)=>{currentlyReserved+=element.roomsNo})
+      let willBeAvailable = 0;
+      if(validRooms.length === 0){
+        let notAffectingReserv = await this.ReservationModel.find({checkInDate :{$gte : createReservationDto.checkOutDate,$ne : createReservationDto.checkOutDate},roomId:createReservationDto.roomId})
+        let notAffectingRoomsNo = 0;   
+        notAffectingReserv.forEach((element:CreateReservationDto)=>{notAffectingRoomsNo+=element.roomsNo})
+        currentlyReserved -=  notAffectingRoomsNo
+        willBeAvailable = roomToReserve.quantity - currentlyReserved
+        if(willBeAvailable < 1){return {message:`we dont have rooms from  this type At this time`}}
+        if(willBeAvailable < createReservationDto.roomsNo) return {message:`we only have ${roomToReserve.quantity}  rooms from  this type At this time`}
+      }else{
+        let available =  roomToReserve.quantity - currentlyReserved
+        validRooms.forEach((element:CreateReservationDto) => {willBeAvailable+= element.roomsNo})
+        available += willBeAvailable 
+        if(available < 1)return {message:`we dont have rooms from  this type At this time`}
+        if(available < createReservationDto.roomsNo)return {message:`we only have ${available}  rooms from  this type At this time`}
+      
+      }
     }
-    if(willBeAvailable < createReservationDto.roomsNo)return {message:"Room Not Available At this time"}
-    let newReservation = await this.reserve(createReservationDto,roomToReserve)
+    return {message:undefined}
+  }
+  async create(createReservationDto: CreateReservationDto) {
+    // let validRooms= await this.ReservationModel.find({checkOutDate :{$lte : createReservationDto.checkInDate},roomId:createReservationDto.roomId})
+    // if(validRooms.length === 0)return {message:"Room Not Available At this time"}
+    // let willBeAvailable = 0;
+    // let willBeAvailable = roomToReserve.quantity;
+    // if(roomToReserve.quantity < createReservationDto.roomsNo){
+      // let validRooms= await this.ReservationModel.find({checkOutDate :{$lte : createReservationDto.checkInDate},roomId:createReservationDto.roomId})
+      // if(validRooms.length === 0)return {message:"Room Not Available At this time"}
+      // validRooms.forEach((element:CreateReservationDto) => {willBeAvailable+= element.roomsNo})
+    // }
+    // if(willBeAvailable < createReservationDto.roomsNo)return {message:"Room Not Available At this time"}
+    let Available = (await this.checkAvailability(createReservationDto)).message
+    if(Available) return {message:Available}
+    let newReservation = await this.reserve(createReservationDto)
+    // let newReservation = await this.reserve(createReservationDto,roomToReserve)
     return {message:"Reservation Success", data:newReservation};
   }
 
@@ -51,18 +87,20 @@ export class ReservationService {
   }
 
   async update(id: number, updateReservationDto: UpdateReservationDto) {
+    let Available = (await this.checkAvailability(updateReservationDto)).message
+    if(Available) return {message:Available}
     let updatedReservation = await this.ReservationModel.findByIdAndUpdate(id,updateReservationDto,{new:true,runValidators:true})
     if(!updatedReservation)return {message:"Update Failed! Please try again later."}
     return {message:"Updated Successfully", data:updatedReservation};
   }
 
   async remove(id: number) {
-    const removedReservation = await this.ReservationModel.findById(id);
+    // const removedReservation = await this.ReservationModel.findById(id);
     await this.ReservationModel.findByIdAndDelete(id)
-    let{ roomsNo, roomId } = removedReservation
-    let updatedRoom = await this.roomService.findOne(roomId)
-    updatedRoom.quantity += roomsNo
-    await this.roomService.update(roomId,updatedRoom)
+    // let{ roomsNo, roomId } = removedReservation
+    // let updatedRoom = await this.roomService.findOne(roomId)
+    // updatedRoom.quantity += roomsNo
+    // await this.roomService.update(roomId,updatedRoom)
     let newData = await this.ReservationModel.find({});
     return {message:"Deleted Successfully", data:newData};
   }
